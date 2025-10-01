5734ab971b66ddc3d4ebf211bdd46cd0f561e9a3
cevikb <61644646+cevikb@users.noreply.github.com>
2025-10-01 13:05:25 +0300

    Update 03_package_bwa_index.sh

--- 03_package_bwa_index.sh
+++ 03_package_bwa_index.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# Step 3: Package BWA Reference Genome Index
-# Platform: Windows Command Prompt (cmd.exe)
-# Note: Example shown in bash format for reproducibility, 
-# but originally executed on Windows cmd.exe.
-
-tar -czvf Ref.bwa-index.tar.gz -C parabricks_sample/Ref 
+tar -czvf Ref.bwa-index.tar.gz -C parabricks_sample/Ref
